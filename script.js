15b0d4551d1e2a6040c871f10be8ff7c6e4770e5
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2025-01-02 23:17:14 -0800

    completed my add task function along with my create task function

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 
 const addButton = document.getElementById('addTask');
-const taskInput = document.getElementById('taskInput');
+const taskInput = document.getElementById('taskInput'); //grabs the 'input' in html file
 const taskList = document.getElementById('taskList');
+
+function addTask () {
+
+  const task = taskInput.value.trim();
+
+  if (task) {
+    createTaskElement(task);
+    taskInput.value = '';          //this if statement is clearing your add box after somethings been inputed to the list. 
+    saveTask();       
+  } else {                         //if nothin is put in it will return this alert below.
+    alert('Please enter a task')
+  }
+
+}
+
+addButton.addEventListener('click', addTask)  //this is adding an event listener that waits  untill 'add' is clicked then 
+// proceeds with the following function below
+
+
+function createTaskElement(task) {
+  const listItem = document.createElement('li') //this grabs the item that is enbedded in the ul and calls it as a li
+
+  listItem.textContent = task; //this is saying that the text in the li will display as a task
+
+  taskList.appendChild(listItem);
+}
+
+//saving task to local storage
+function saveTask() {
+
+  let task = [];
+
+  taskList.querySelectorAll('li').forEach(function(item) {
+    task.push(item.textContent.trim());
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(task))
+}
